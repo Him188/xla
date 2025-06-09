309de408aa245fcfcf8e33ded20a89e4f7dd07d4
Him188 <Him188@mamoe.net>
2025-06-09 01:06:11 +0100

    add SimpleDotSimpleDotBug

--- xla/tests/concurrency_trace/cross_stream_synchronization_tests.cc
+++ xla/tests/concurrency_trace/cross_stream_synchronization_tests.cc
@@ -21,6 +21,23 @@ ENTRY test_graph {
           false);
 }
 
+XLA_TEST_F(CrossStreamSynchronizationTests, SimpleDotBug) {
+  EnableSyntheticWaitForStreamsBug();
+  RunTest(R"(
+HloModule test_graph
+
+ENTRY test_graph {
+  p0 = f32[2048,2048]{1,0} parameter(0)
+  p1 = f32[2048,2048]{1,0} parameter(1)
+  dot0 = f32[2048,2048]{1,0} dot(p0, p1), lhs_contracting_dims={1}, rhs_contracting_dims={0}
+  dot1 = f32[2048,2048]{1,0} dot(dot0, p1), lhs_contracting_dims={1}, rhs_contracting_dims={0}
+  sub = f32[2048,2048]{1,0} subtract(dot1, dot0)
+  ROOT out = (f32[2048,2048]{1,0}) tuple(sub)
+}
+  )",
+          false);
+}
+
 XLA_TEST_F(CrossStreamSynchronizationTests, AsyncDot) {
   EnableAsyncDot();
   RunTest(R"(
